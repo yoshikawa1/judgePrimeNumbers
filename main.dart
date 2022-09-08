d891e56fffb5723d7a7bd4ef69e92e64b613253d
yoshikawa1 <110716777+yoshikawa1@users.noreply.github.com>
2022-09-08 11:33:44 +0900

    Update main.dart
    誤ったソースになった為、正しい状態に戻す。

--- main.dart
+++ main.dart
@@ -5,23 +5,12 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
+  const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
@@ -30,86 +19,90 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
   final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
+  void _judgePrimeNumbers(String inputText) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      int input = int.parse(inputText);
+      int counter = 0;
+      var list = <int>[];
+      for (int i = 1; i <= input; i++) {
+        if (input % i == 0) {
+          counter++;
+          list.add(i);
+        }
+      }
+      if (counter == 2) {
+        outputSummary = '$inputText  は素数です';
+      } else {
+        outputSummary = '$inputText  は素数ではありません';
+      }
+      outputList = '役数は $list';
     });
   }
 
+  void _clear() {
+    setState(() {
+      outputSummary = '';
+      outputList = '';
+    });
+  }
+
+  String outputSummary = '';
+  String outputList = '';
+  final myController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('素数判定'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Form(
+        key: _formKey,
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          children: [
+            TextFormField(
+              style: const TextStyle(fontSize: 20),
+              controller: myController,
+              decoration: const InputDecoration(
+                hintText: '正の整数を入力してください',
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'テキストを入力してください';
+                } else {
+                  if (RegExp(r'^[1-9]{1}[0-9]*$').hasMatch(value.trim())) {
+                    return null;
+                  } else {
+                    return '正の整数を入力してください';
+                  }
+                }
+              },
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            ElevatedButton(
+              child: const Text('素数判定'),
+              onPressed: () {
+                final input = myController.text;
+                if (_formKey.currentState!.validate()) {
+                  _formKey.currentState!.save(); // TextFormFieldのonSavedが呼び出される
+                  _judgePrimeNumbers(input);
+                } else {
+                  _clear();
+                }
+              },
             ),
+            Text(outputSummary, style: const TextStyle(fontSize: 20)),
+            Text(outputList, style: const TextStyle(fontSize: 20))
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
